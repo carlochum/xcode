5b59321da6d933fe70894f1b6bdcef13bf91d436
carlochum <Carlo@Carlos-MacBook-Air.local>
2014-08-14 16:24:25 +1000

    Initialisation
    Ignored files etc.

---
+++ palindrome/palindrome/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  palindrome
+//
+//  Created by Carlo Chumroonridhi on 14/08/2014.
+//  Copyright (c) 2014 id11000989. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
